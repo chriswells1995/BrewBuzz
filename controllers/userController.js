11a9c23b091372e00397806bfeb0d0c33679fb08
Chris W <wells457@umn.edu>
2020-04-15 11:09:58 -0500

    After testing deployed APIs, User's post route was fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,20 +29,7 @@ router.get("/api/user/:id", function(req, res) {
     .catch(err => res.status(500).json(err))
   });
 
-// Add a User
-router.post("/api/user", function(req, res) {
-  // reference the User model and then utilize the sequelize.create
-  // built in method to create a new User
-  // inside .create() we reference the author and body columns we feed
-  // those columns values from req.body
-  db.User.create({
-    // adding req.body(response) to name, website, streetAddress
-    name: req.body.name,
-    website: req.body.website,
-    streetAddress: req.body.streetAddress
-  }).then(() => res.json(true))
-    .catch((err) => res.status(500).json(err))
-});
+
 
 
 module.exports = router;
