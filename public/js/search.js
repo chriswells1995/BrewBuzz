a311c0e9c61428dcd0a3df839a1a6d5222c55213
Chris W <wells457@umn.edu>
2020-04-16 15:26:05 -0500

    Can now search for a brewery, render potential breweries to the page, and click a button to POST one of those breweries to out database

--- public/js/search.js
+++ public/js/search.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-    // tie an event listener to the search bar for every HTML
-    // get the search term from the input in the search bar
-    // make a GET ajax call that returns breweries based on a search term
-    // render those breweries to the page. Temporarily as buttons, but in the future more fancy. 
-    // when clicking on those buttons, tie an event listener that calls the ajax with the properties of that specific object
-    // make a POST ajax call, adding that brewery's name, website, and streetAddress to the database
+    // DONE tie an event listener to the search bar for every HTML 
+    // DONE get the search term from the input in the search bar
+    // DONE make a GET ajax call that returns breweries based on a search term
+    // DONE render those breweries to the page. Temporarily as buttons, but in the future more fancy. 
+    // DONE when clicking on those buttons, tie an event listener that calls the ajax with the properties of that specific object
+    // DONE make a POST ajax call, adding that brewery's name, website, and streetAddress to the database
     
 
 
-// This is a global variable we may want to put the full response in 
+// This is a global variable we may want to put the full response in. Currently not needed.
 var FullArray=[];
 
 
@@ -34,34 +34,22 @@ var FullArray=[];
             // the full response is stored in the global FullArray
             FullArray=response;
             console.log('FullArray: ', FullArray)
-            // a for loop puts all the brewery names in one breweryNames array
-            var breweryNames=[];
-            var breweryStreet=[];
-            var breweryCity=[];
-            var breweryWebsites=[];
+            // a for loop puts all the brewery properties we need in Objects, then pushed into an array of those objects
             var BreweryObject={};
             var AllBreweryObjects=[];
             for (i=0; i<response.length; i++){
-                // breweryNames.push(response[i].name) ;
-                // breweryStreet.push(response[i].street);
-                // breweryCity.push(response[i].city)
-                // breweryWebsites.push(response[i].website_url)
-                // breweryAddress=(breweryStreet[i] + ", " + breweryCity[i]);
-                // console.log(breweryAddress)
 
                 BreweryObject={
                      breweryName: response[i].name,
                     //  breweryStreet: breweryStreet[i],
                     //  breweryCity: breweryCity[i],
                     breweryAddress: response[i].street + ", " + response[i].city + ", " + response[i].state,
-                    breweryWebsite: response[i].website_url       
+                    breweryWebsite: response[i].website_url,
+                    OpenBreweryID: response[i].id       
                 }
                 AllBreweryObjects.push(BreweryObject);
             }
 
- 
-
-            // console.log("names: ", breweryNames)
             console.log("objects: ",AllBreweryObjects)
 
 
@@ -80,23 +68,23 @@ var FullArray=[];
 
 
 
-        // TODO: instead of buttons, make a nice looking list of breweries with their address. 
+        // Render a list of breweries with their address. 
         // then check if they are in our database yet, and if not, THEN put a message under that brewery that says,
         // "No reviews yet! Click Here to make the first!" with the "Click Here" on a button that POSTS that brewery to our databse.
+         // TODO: If a brewery IS in our database, attach Event Listeners and a href's to Brewery Names so clicking them will bring up brewery.html, and set off a render function for that page
 
-        // CURRENT METHOD WE'RE USING FOR TESTING
-        // then we create the AddBreweryButton variable, which we'll use to create a <button> element, "Button-add" class, 
-        // and add the brewery Names through a for loop, and append these buttons
+
         var AddBreweryButton;
         var renderedBreweryName;
         var renderedBreweryAddress;
         var lineBreak=$("<br>"); 
         for (i = 0; i < AllBreweryObjects.length; i++) {
 
-
+            // create h2's that hold the brewery name
             renderedBreweryName=$("<h2>");
             renderedBreweryName.text(AllBreweryObjects[i].breweryName)
             
+            // create h3's that hold the brewery address
             renderedBreweryAddress=$("<h3>")
             renderedBreweryAddress.text(AllBreweryObjects[i].breweryAddress)
 
@@ -107,27 +95,73 @@ var FullArray=[];
             $("#OpenBreweries").append(renderedBreweryAddress)
 
             // TODO: Create an IF statement that checks if these breweries exist in our current database. If not, do this.
+
+            // create buttons with "AddOpenBrewery" class, and value of the index number in the list of breweries
             AddBreweryButton = $("<button>")
-            AddBreweryButton.attr("class", "Button-add")
+            AddBreweryButton.attr("value", i)//AllBreweryObjects[i].OpenBreweryID)
             AddBreweryButton.text("Click Here to Write Their First Review!")
+            AddBreweryButton.attr("class", "AddOpenBrewery")
+
+            
             // Append AddBreweryButton
             $("#OpenBreweries").append(AddBreweryButton);
 
-            // Append a Line Break to sepereate eveything
-            // $("#OpenBreweries").append(lineBreak);
-            // $("#OpenBreweries").append(lineBreak);
-            // $("#OpenBreweries").append(lineBreak);
 
 
             
         }
-        // Currently, this will create a bunch of buttons with the names of breweries that match the search result. 
-        // TODO: create an assignClick function and call it here. This function will put event listeners on everything
-        // New breweries will have buttons that make an ajax call posting that brewery to our databse
-        // Breweries already in our databse will have links to their brewey HTML pages, and make a GET request for the reviews of that brewery.
 
+        // call assign click function to add event listeners
+        assignClick(AllBreweryObjects)
+
+        // End of render function
     }
     
+    // adds event listeners to newly created buttons
+    function assignClick(AllBreweryObjects) {
+
+        // event listeners on all "AddOpenBrewery" buttons
+            $(".AddOpenBrewery").on("click", function (event) {
+
+            event.preventDefault()
+
+            console.log(event.target.value)
+            console.log(AllBreweryObjects[event.target.value]);
+    
+    
+
+
+                // POST ajax call using the event.target.value (from the button we clicked) as an index number, signifying it's place in the AllBreweryObjects array,
+                // So we can use that brewery's info in the ajax.
+
+            var settings = {
+                "url": "/api/brewery",
+                "method": "POST",
+                "timeout": 0,
+                "headers": {
+                  "Content-Type": "application/x-www-form-urlencoded"
+
+                },
+                "data": {
+                  "name": AllBreweryObjects[event.target.value].breweryName,
+                  "website": AllBreweryObjects[event.target.value].breweryWebsite,
+                  "streetAddress": AllBreweryObjects[event.target.value].breweryAddress
+                }
+              };
+
+              console.log(settings)
+              
+              $.ajax(settings).then(function (response) {
+                console.log(response);
+              });
+
+    
+        })
+
+        
+
+    
+    }
 
 
     // end of document listener
