f0eaf3095b2de5c77969e6705b74f7242a4e69c8
RAW <rwilliams01101@gmail.com>
2020-04-16 14:55:32 -0500

    updated API routes for deployment

---
+++ public/js/eventListeners.js
@@ -0,0 +1,6 @@
+$("#headerName").on("click", function() {
+    var query = $(brewery);
+    console.log("BREWERYHERE", brewery)
+    // return if they didn't put anything in the form-controlbox
+    return query;
+    });
