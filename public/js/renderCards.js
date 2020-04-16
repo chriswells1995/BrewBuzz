da082ebd428904c374b5243db0ad80fd068fe759
RAW <rwilliams01101@gmail.com>
2020-04-16 12:11:11 -0500

    forloop completed for homepage, rendering 5 reviews from JSON

--- public/js/renderCards.js
+++ public/js/renderCards.js
@@ -1,47 +1,53 @@
 function displayCards() {
   //  console.log(searchResult);
   var settings = {
-    "url": "http://localhost:8080/api/reviews/",
-    "method": "GET",
-    "timeout": 0
+    url: "http://localhost:8080/api/reviews/",
+    method: "GET",
+    timeout: 0,
     // "headers": {
     //   "Cookie": "connect.sid=s%3AWonUzQhp09cdIZjjmEUsVh6Z5lDyP72o.Zfm8WwKpzjxd7kcyBTbnGxlDXsNNIEerisDXqBVqFSg"
     // },
   };
-  $.ajax(settings)
-  .then(function(response) {
-      // console.log(response);
-    var responseData = response
-    console.log(responseData)
-    var review = responseData[4].User.email
-  
+  $.ajax(settings).then(function (response) {
+    // console.log(response);
+    var responseData = response;
+    console.log(responseData);
+    for (let i = 0; i < 5; i++) {
+      var review = responseData[i].review;
+      var email = responseData[i].User.email;
+      var brewery = responseData[i].Brewery.name;
+      buildCard(brewery, email, review);
+    }
+    // [3]
+
     // var breweryName = [review][0]['User'][0]
-    console.log("Review: ", review);
-    // console.log("email", breweryName);
-    buildCard(review)
-    // console.log("User: ", User);
-    // for testing
-    
+    console.log(
+      "Review: ",
+      review,
+      "\n",
+      "Email: ",
+      email,
+      "\n",
+      "Brewery: ",
+      brewery
+    );
+
     // for (let [reviewID, reviewValues] of Object.entries(responseData)) {
-      //   console.log("reviewID", reviewID)
-      //   console.log("reviewvalues", reviewValues)
-      //   for (let [reviewDisplayValues] of Object.entries(reviewValues)) {
-        //     buildCard(reviewID, reviewDisplayValues);
-        //   }
-        // }
-      });
-      
-      function buildCard(review, review_id, reviewDisplayValues) {
-        var email = 'rwilliams01101@gmail.com'
-        var name = 'Surly'
-    // var { name, email, review } = reviewDisplayValues;
-    var cardDiv = $("<div>")
-      .addClass("col-sm-12 coinCard " + review_id)
-      .attr("id", review_id);
+    //   console.log("reviewID", reviewID)
+    //   console.log("reviewvalues", reviewValues)
+    //   for (let [reviewDisplayValues] of Object.entries(reviewValues)) {
+    //     buildCard(reviewID, reviewDisplayValues);
+    //   }
+    // }
+  });
+
+  function buildCard(brewery, email, review) {
+    var cardDiv = $("<div>").addClass("col-sm-12 coinCard ");
+    // .attr("id", review_id);
 
     var cardBrewery = $("<h4>")
       .addClass("card-header")
-      .text("Brewery: " + name); // this will display the brewery 
+      .text("Brewery: " + brewery); // this will display the brewery
 
     var cardUser = $("<div style=font-size:125%;>")
       .attr("id", "cardBack")
@@ -60,15 +66,10 @@ function displayCards() {
       )
       .addClass("card-body"); // this probably won't be needed but could potentially be used on other pages
 
-    cardDiv.append(
-      cardBrewery,
-      cardUser,
-      cardReview,
-      deleteBtn
-    );
+    cardDiv.append(cardBrewery, cardUser, cardReview, deleteBtn);
 
     $("#add-reviewcards").append(cardDiv);
   }
 }
 
-displayCards()
+displayCards();
