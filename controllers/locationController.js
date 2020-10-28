80ef0af7b01ac426210dbc45fbae3d6be9a0e7d0
Lukas Hirsch <hirschlukasa@gmail.com>
2020-10-28 15:07:05 -0500

    incognito is your friend. req.query works to populate user coords

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -7,10 +7,17 @@ const db = require("../models");
 // Routes
 // =============================================================
 router.get(`/api/nearby`, function (req, res) {
-    console.log("nearby get")
-    db.sequelize.query("select * FROM breweries WHERE (latitude > 32.2393 AND latitude < 34.0393) AND (longitude > -87.000 AND longitude < -85.9)",{raw: true, type: db.sequelize.QueryTypes.SELECT})
-    .then(results => res.json(results))
-        .catch(error => res.json(error))
+    //let minLat = (parseInt(req.body.userLat) -.3);
+    console.log('req full :',req)
+    console.log('get request lat :', req.query);
+    //console.log('minlat : ',minLat)
+    let maxLat = req.body.userLat;
+    let lon = req.body.userLon
+    console.log("route lat :", req.query.userLat)
+    console.log("route lon : ", req.query.userLon)
+    // db.sequelize.query(`select * FROM breweries WHERE (latitude > (${minLat}) AND latitude < ${minLat}) AND (longitude > -87.000 AND longitude < -85.9)`,{raw: true, type: db.sequelize.QueryTypes.SELECT})
+    // .then(results => res.json(results))
+    //     .catch(error => res.json(error))
     })
 
 
