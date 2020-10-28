08d4ae56c347ad227fd79279c6873df55405b21a
Lukas Hirsch <hirschlukasa@gmail.com>
2020-10-28 12:28:20 -0500

    sequelize IS defined

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -6,12 +6,17 @@ const db = require("../models");
 
 // Routes
 // =============================================================
-router.get(`/api/nearby`, function(req,res){
+router.get(`/api/nearby1`, function (req, res) {
     console.log("nearby get")
-    db.sequelize.query("select * FROM breweries WHERE (latitude > 32.2393 AND latitude < 34.0393) AND (longitude > -87.000 AND longitude < -85.9)",{type: sequelize.QueryTypes.SELECT})
-        .then(results => res.json(results))
-        // .catch(error => res.json(error))
-});
+    db.sequelize.query("select * FROM breweries WHERE (latitude > 32.2393 AND latitude < 34.0393) AND (longitude > -87.000 AND longitude < -85.9)",{raw: true, type: db.sequelize.QueryTypes.SELECT})
+    .then(results => res.json(results))
+        .catch(error => res.json(error))
+    })
+    // .catch(error => res.json(error))
 
+
+// sequelize.query('SELECT...').spread((results, metadata) => {
+//     // Raw query - use spread
+//   });
 
 module.exports = router;
