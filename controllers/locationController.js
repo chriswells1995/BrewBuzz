be5813c961cd3e91d40e580f69d3ca1ea7dc60bf
Lukas Hirsch <hirschlukasa@gmail.com>
2020-10-28 15:40:23 -0500

    query works in mysql, not returning json

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -15,14 +15,22 @@ router.get(`/api/nearby`, function (req, res) {
     console.log('intlat ' , intLat)
     let maxLat = (intLat + .2);
     let minLat = (intLat - .2);
-    console.log(`user lat ${req.query.userLat}  maxLat ${maxLat}  minLat ${minLat}`)
+    let intLon = parseFloat(req.query.userLon)
+    console.log('intLon ' , intLon)
+    let maxLon = (intLon + .2);
+    let minLon = (intLon - .2);
+    console.log(`user lat ${req.query.userLat}  maxLat ${maxLat}  minLat ${minLat} user Lon ${req.query.userLon}  maxLon ${maxLon}  minLon ${minLon}`)
     let lon = req.body.userLon
     console.log("route lat :", req.query.userLat)
     console.log("route lon : ", req.query.userLon)
-    // db.sequelize.query(`select * FROM breweries WHERE (latitude > (${minLat}) AND latitude < ${minLat}) AND (longitude > -87.000 AND longitude < -85.9)`,{raw: true, type: db.sequelize.QueryTypes.SELECT})
-    // .then(results => res.json(results))
-    //     .catch(error => res.json(error))
+     db.sequelize.query(`select * FROM breweries WHERE (latitude > ${minLat} AND latitude < ${maxLat}) AND (longitude > ${minLon} AND longitude < ${maxLon})`,{raw: true, type: db.sequelize.QueryTypes.SELECT})
+     .then(function(results){ 
+        console.log('sql results', results) 
+        res.json(results)})
+        .catch(error => res.json(error))
     })
 
 
 module.exports = router;
+
+
