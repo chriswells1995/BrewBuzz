8ce979ad7f6de2d342a8dc5fcea9ef8c34e549ec
RAW <rwilliams01101@gmail.com>
2020-10-29 12:00:29 -0500

    commits to push to master

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,42 +1,46 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/landing.html",
-    "/login.html",
-    "/signup.html",
-    "/user.html",
-    "/brewery.html",
-    "/search.html",
-    "/forgot-password.html",
-    "/resetpassword.html",
-    "/js/flagBrewery.js",
-    "/js/login.js",
-    "/js/renderBrewery.js",
-    "/js/renderCards.js",
-    "/js/renderSearch.js",
-    "/js/resetPassword.js",
-    "/js/search.js",
-    "/js/sharer.js",
-    "/js/signup.js",
-    "/js/userCard.js",
-    "/manifest.webmanifest",
-    "/stylesheets/style.css",
-    "/stylesheets/assets/BrewBuzz_Icon_Clear.png",
-    "/stylesheets/assets/BrewBuzz_Banner_Clear.png",
-    "/stylesheets/assets/cheers.png",
-    "/stylesheets/assets/Honeycomb_Light.png",
-    "/stylesheets/assets/Honeycomb_Text_Background.png",
-    "/stylesheets/assets/Honeycomb.png",
-    "/stylesheets/assets/No_Reviews.png",
-    "/stylesheets/assets/OneofUs.png"
-  ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
+  "/",
+  "/brewery.html",
+  "/forgot-password.html",
+  "/landing.html",
+  "/login.html",
+  "/nearby.html",
+  "/resetpassword.html",
+  "/signup.html",
+  "/search.html",
+  "/user.html",
+  "/userlanding.html",
+  "/js/flagBrewery.js",
+  "/js/geoTracking.js",
+  "/js/login.js",
+  "/js/nearby.js",
+  "/js/renderBrewery.js",
+  "/js/renderCards.js",
+  "/js/renderSearch.js",
+  "/js/resetPassword.js",
+  "/js/search.js",
+  "/js/sharer.js",
+  "/js/signup.js",
+  "/js/userCard.js",
+  "/manifest.webmanifest",
+  "/stylesheets/style.css",
+  "/stylesheets/assets/BrewBuzz_Icon_Clear.png",
+  "/stylesheets/assets/BrewBuzz_Banner_Clear.png",
+  "/stylesheets/assets/cheers.png",
+  "/stylesheets/assets/Honeycomb_Light.png",
+  "/stylesheets/assets/Honeycomb_Text_Background.png",
+  "/stylesheets/assets/Honeycomb.png",
+  "/stylesheets/assets/No_Reviews.png",
+  "/stylesheets/assets/OneofUs.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 // install
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       console.log("Your files were pre-cached successfully!");
       return cache.addAll(FILES_TO_CACHE);
     })
@@ -46,11 +50,11 @@ self.addEventListener("install", function(evt) {
 });
 
 // activate
-self.addEventListener("activate", function(evt) {
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
-    caches.keys().then(keyList => {
+    caches.keys().then((keyList) => {
       return Promise.all(
-        keyList.map(key => {
+        keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
@@ -64,25 +68,29 @@ self.addEventListener("activate", function(evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
+self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
 
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
 
     return;
-}});
+  }
+});
