77aafbe652142e0a22e9bd8a66ca6eeaa07ceb67
Chris W <wells457@umn.edu>
2020-11-10 00:48:55 -0600

    added simple redirect for https

---
+++ public/js/redirect.js
@@ -0,0 +1,9 @@
+console.log("=====================================")
+console.log(window.location.href)
+
+if(!window.location.href.includes("https")){
+  
+if (!window.location.href.includes("localhost"))
+window.location.href="https://www.brewbuzz.beer"
+
+}
